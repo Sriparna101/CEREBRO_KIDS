c354c6ed8d698b0854b304e9858dfc223e54bef6
Shubham Patni <39957304+shubham8patni@users.noreply.github.com>
2020-07-15 14:14:01 +0530

    Add files via upload

--- registercustomer.php
+++ registercustomer.php
@@ -15,18 +15,6 @@
 					First name:<br> <input type="text" name="firstname" ><br><br>
 					Last name: <br><input type="text" name="lastname" ><br><br>
 					Email: <br><input type="email" name="email" ><br><br>
-				
-					
-					Cusine Preference:<br>
-					<input type="checkbox" name="cusine[]" value="Chinese">Chinese<br>
-					<input type="checkbox" name="cusine[]" value="French">French<br>
-					<input type="checkbox" name="cusine[]" value="Italian">Italian<br>
-					<input type="checkbox" name="cusine[]" value="Indian">Indian<br><br>
-
-					Preference: <br> 
-					<input type="checkbox" name="preference[]" value="Vegetarian">Vegetarian<br>
-					<input type="checkbox" name="preference[]" value="Non-vegetarian">Non-vegetarian<br><br>
-
 
 					Password: <br><input type="Password" name="Password" min="8" ><br>
 					Re-Type Password: <br><input type="Password" name="RePassword" ><br><br>
