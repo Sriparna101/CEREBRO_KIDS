c722526fafc03a4cc14f5c2ec0b113639f557bd5
Abhinav <abhinavrgupta98@gmail.com>
2020-07-15 23:19:53 +0530

    adjustments

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" type=text/css href="./stylesheets/style.css">
     <link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
     <link rel="stylesheet" type="text/css" href="./stylesheets/assign.css">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="./js/index.js"></script>
@@ -18,17 +18,23 @@
 
 <body>
     <div class="header">
+    <div class="headerSection1">
         <img id="ckLogo" src="./ck_logo.png" alt="cerebro kids">
         <div class="verticalBar"></div>
         <div id="contactInfo">
-            <span>hello@cerebrokids.com</span>
-            <span>+91 99717 12678</span>
+            <div>hello@cerebrokids.com</div>
+            <div>+91 99717 12678</div>
         </div>
-        <p id="headerTitle">TITAN</p>
-        <div id="userInfo">
-            <!-- <span>Hello User</span> -->
+    </div>
+    <div class="headerSection2">
+        TITAN
+    </div>
+    <div id="userInfo" class="headerSection3">
+            <span >Hello Abhinav Gupta</span>
             <img src="./icon.jpeg" alt="user">
-        </div>
+    </div>
+        
+        
     </div>
     <button class="openBtn">OPEN MENU</button>
     <button class="closeBtn">CLOSE MENU</button>
@@ -56,102 +62,7 @@
         </div>
 
     <div id="mainContent">
-        <div id=" article" class="wrapper">
-                    <div id="guts" class="contactus">
-                                    
-                        
-                                        <!--    <nav class="navbar-nav navbar-expand-sm fixed-navbar">
-                                                <ul class="navbar-nav mr-auto">
-                                                    <li class="nav-item ni"><a class="nav-link nvr" href="video.php">VIDEOS</a></li>
-                                                    <li class="nav-item blank"></li>
-                                                    <li class="nav-item ni"><a class="nav-link nvr" href="worksheet.php">WORKSHEET</a></li>
-                                                    <li class="nav-item blank"></li>
-                                                    <li class="nav-item ni"><a class="nav-link nvr" href="article.php">ARTICLES</a></li>
-                                                </ul>
-                                            </nav>
-                                            <!--<div class="col cs4gr"><a class="nav-link disabled" style="color:white;">VIDEOS</a></div>
-                                            <div class="blank"></div>
-                                            <div class="col cs4at"><a class="nav-link nlrat" href="worksheet.php">WORKSHEET</a></div>
-                                            <div class="blank"></div>
-                                            <div class="col cs4at"><a class="nav-link nlrat" href="article.php">ARTICLES</a></div>-->
         
-                                        <div>
-                                            <div  style="color:white;">Module - 1</div>
-                                        </div><br>
-                                        <div class="horizontal-scroll ml-1 mt-2">
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                        </div><br>
-                                        <div>
-                                            <div class="col" style="color:white;">Module - 2</div>
-                                        </div><br>
-                                        <div class="horizontal-scroll ml-1 mt-3">
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                        </div><br>
-                                        <div>
-                                            <div class="col" style="color:white;">Module - 3</div>
-                                        </div><br>
-                                        <div class="horizontal-scroll ml-1 mt-3">
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                                <video width="320" height="240" controls>
-                                                  <source src="movie.mp4" type="video/mp4">
-                                                  <source src="movie.ogg" type="video/ogg">
-                                                Your browser does not support the video tag.
-                                                </video>
-                                        </div>
-                                    </div>
-                    </div>
 
     </div>
     <footer>
@@ -163,7 +74,7 @@
 
 
 </body>
-<script>
+<!-- <script>
     const sidebar = document.querySelector('.sideNavbar');
     const btns = document.querySelectorAll('.sideNavbar-btn');
 
@@ -178,6 +89,6 @@
         });
     });
         //PAGE LOAD
-</script>
+</script> -->
 
 </html>
