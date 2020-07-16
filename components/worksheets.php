93bbc9e5d47ceaf066e421de6d3ac0d46f5a2212
Abhinav <abhinavrgupta98@gmail.com>
2020-07-17 01:17:53 +0530

    all recovered

---
+++ components/worksheets.php
@@ -0,0 +1,74 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./stylesheets/worksheets.css">
+    
+</head> -->
+
+<body>
+    <div class="wrapper">
+        <div class="money assignCol">
+            <h3>Money</h3>
+            <ul>
+                <li>Fun Currency    <i class="fa fa-download downloadButton"></i>​​</li>
+                <li>Let’s Barter​    <i class="fa fa-download downloadButton"></i></li>
+                <li>Joy of Giving    <i class="fa fa-download downloadButton"></i></li>
+            </ul>
+        </div>
+
+        <div class="budgeting assignCol">
+            <h3>Budgeting</h3>
+            <ul>
+                <li>Budget Planner    <i class="fa fa-download downloadButton"></i>​</li>
+                <li>Ask your Parents​​    <i class="fa fa-download downloadButton"></i></li>
+            </ul>
+        </div>
+
+        <div class="saving assignCol">
+            <h3>Saving</h3>
+            <div>
+                ABC
+            </div>
+        </div>
+        <div class="investment assignCol">
+            <h3>Investment</h3>
+            <div>
+                ABC
+            </div>
+        </div>
+
+        <div class="taxes assignCol">
+            <h3>Taxes</h3>
+            <div>
+                ABC
+            </div>
+        </div>
+
+        <div class="banking assignCol">
+            <h3>Banking</h3>
+            <div>
+                ABC
+            </div>
+        </div>
+
+        <div class="digitalFinance assignCol">
+            <h3>Digital Finance</h3>
+            <div>
+                ABC
+            </div>
+        </div>
+        <div class="moneyFrauds assignCol">
+            <h3>Money Frauds</h3>
+            <div>
+                ABC
+            </div>
+        </div>
+    </div>
+<!-- 
+
+</body>
+
+</html> -->
---
+++ js/index.js
@@ -0,0 +1,100 @@
+
+//  SECTION 4 LOADING
+
+$(document).ready(function () {
+    $("#assign").click(function () {
+        $(".sideNavbar-btn").removeClass('active');
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+
+        $(this).addClass('active');
+        $('#mainContent').empty();
+        $("#mainContent").load('components/assignments.php');
+    });
+    $("#profile").click(function () {
+        $(".sideNavbar-btn").removeClass('active');
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+
+        $(this).addClass('active');
+        $('#mainContent').empty();
+        $("#mainContent").load('components/profilepage.php');
+    });
+    $("#ffk").click(function () {
+        $(".sideNavbar-btn").removeClass('active');
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+
+        $(this).addClass('active');
+        $('#mainContent').empty();
+        $("#mainContent").load('components/financeforkids.php');
+    });
+    $("#backup").click(function () {
+        $(".sideNavbar-btn").removeClass('active');
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+
+        $(this).addClass('active');
+        $('#mainContent').empty();
+        $("#mainContent").load('components/backupclasses.php');
+    });
+    $("#contact").click(function () {
+        $(".sideNavbar-btn").removeClass('active');
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+
+        $(this).addClass('active');
+        $('#mainContent').empty();
+        $("#mainContent").load('components/contactus.php');
+    });
+});
+
+// TOP NAV BAR ACTION
+$(document).ready(function () {
+    $("#mainContent").load('components/backupclasses.php');
+    $("#topBtn-videos").addClass("activeTopbarBtn");
+
+    $("#topBtn-videos").click(function () {
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+        $(".sideNavbar-btn").removeClass('active');
+
+        $(this).addClass("activeTopbarBtn");
+        $('#mainContent').empty();
+        $("#mainContent").load('components/backupclasses.php');
+
+    });
+    $("#topBtn-worksheets").click(function () {
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+        $(".sideNavbar-btn").removeClass('active');
+
+        $(this).addClass("activeTopbarBtn");
+        $('#mainContent').empty();
+        $("#mainContent").load('components/worksheets.php');
+
+    });
+    $("#topBtn-articles").click(function () {
+        $(".topNavbar-btn").removeClass("activeTopbarBtn");
+        $(".sideNavbar-btn").removeClass('active');
+
+        $(this).addClass("activeTopbarBtn");
+        $('#mainContent').empty();
+        $("#mainContent").load('components/articles.php');
+    });
+});
+// SIDE-NAVBAR COLLAPSIBLE BUTTON FOR MOBILE VIEW
+
+
+$(document).ready(function () {
+    $('.openBtn').click(function () {
+        $(this).css({ "display": "none" });
+        $('.closeBtn').css({ "display": "inline-block" });
+        $('.sideNavbar').css({
+            "height": "auto",
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('.closeBtn').click(function () {
+        $(this).css({ "display": "none" });
+        $('.openBtn').css({ "display": "inline-block" });
+        $('.sideNavbar').css({
+            "height": "0",
+        });
+    });
+});
