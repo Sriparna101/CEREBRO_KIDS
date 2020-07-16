9334bace66a21550eac9403bef1cea0f2b1f7a1b
Shubham Patni <39957304+shubham8patni@users.noreply.github.com>
2020-07-16 10:35:02 +0530

    Add files via upload

--- registercustomer.php
+++ registercustomer.php
@@ -12,12 +12,12 @@
 			<h2 class="headingregister">Register</h2>
 			<div class="inner">
 				<form class="formregister" action="index.php" method="post" >
-					First name:<br> <input type="text" name="firstname" ><br><br>
-					Last name: <br><input type="text" name="lastname" ><br><br>
-					Email: <br><input type="email" name="email" ><br><br>
+					First name:<br> <input type="text" name="firstname" required=""><br><br>
+					Last name: <br><input type="text" name="lastname" required=""><br><br>
+					Email: <br><input type="email" name="email" required=""><br><br>
 
-					Password: <br><input type="Password" name="Password" min="8" ><br>
-					Re-Type Password: <br><input type="Password" name="RePassword" ><br><br>
+					Password: <br><input type="Password" name="Password" min="8" required=""><br>
+					Re-Type Password: <br><input type="Password" name="RePassword" required=""><br><br>
 					<button type="submit" value="Register" >Register</button>
 					<button type="reset" >Reset</button>
 				</form>
