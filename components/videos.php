541d43a7e3e038e76edc3137d265200f074caa48
Shubham Patni <39957304+shubham8patni@users.noreply.github.com>
2020-07-22 22:29:06 +0530

    Add files via upload

--- components/videos.php
+++ components/videos.php
@@ -23,7 +23,7 @@
 					
 		
 										<div>
-											<div  class="videoSectionTitle" style="color:white; margin-top: 25px;">Learn-with-Neo</div>
+											<div  class="videoSectionTitle" style="color:white; margin-top: 25px;"><p style="font-size: 30px;">Learn-with-Neo</p></div>
 										</div><br>
 										<div >										
 											
@@ -69,7 +69,7 @@
 
 
 										<div>
-											<div class="col videoSectionTitle" style="color:white;">Learn-with-Experts</div>
+											<div class="col videoSectionTitle" style="color:white;"><p style="font-size: 30px;">Learn-with-Experts</p></div>
 										</div><br>
 										<div >										
 											
@@ -116,7 +116,7 @@
 
 
 										<div>
-											<div class="col videoSectionTitle" style="color:white;">          Learn-it-Yourself</div>
+											<div class="col videoSectionTitle" style="color:white;"><p style="font-size: 30px;">Learn-it-Yourself</p></div>
 										</div><br>
 										<div >										
 											
@@ -128,28 +128,28 @@
 											    </div>
 											  <div class="center" id="content2">
 												<?php
-											require 'connection.php';
-											$list = "";
-											$i="";
-											$query = "SELECT * FROM `videos`";
-											$data = mysqli_query($conn, $query);
-											$row= mysqli_num_rows($data);
-
-												for($i=1 ; $i <= $row ; $i++){
-													$query = "SELECT * FROM `videos` WHERE id=$i"; 
+													require 'connection.php';
+													$list = "";
+													$i="";
+													$query = "SELECT * FROM `videos`";
 													$data = mysqli_query($conn, $query);
-													$result = mysqli_fetch_assoc($data);
+													$row= mysqli_num_rows($data);
 
-													
-														echo "<video width='320' height='240' controls style='margin:5px;' controlslist='nodownload'>";
-														echo "<source src=";
-														echo $result['location'];
-														echo " type='video/mp4'>";
-														echo "Your browser does not support the video tag.";
-														echo "</video>";
-													
-												}
-										?>
+														for($i=1 ; $i <= $row ; $i++){
+															$query = "SELECT * FROM `videos` WHERE id=$i"; 
+															$data = mysqli_query($conn, $query);
+															$result = mysqli_fetch_assoc($data);
+
+															
+																echo "<video width='320' height='240' controls style='margin:5px;' controlslist='nodownload'>";
+																echo "<source src=";
+																echo $result['location'];
+																echo " type='video/mp4'>";
+																echo "Your browser does not support the video tag.";
+																echo "</video>";
+															
+														}
+												?>
 											</div>
 												<div class="rightscroll" style="cursor: pointer;">
 											    	<button id="right-button2">
--- js/scroll.js
+++ js/scroll.js
@@ -1,43 +1,43 @@
 $('#left-button').click(function() {
       event.preventDefault();
       $('#content').animate({
-        scrollLeft: "-=300px"
-      }, "slow");
+        scrollLeft: "-=150px"
+      }, "fast");
 });
    
 $('#right-button').click(function() {
       event.preventDefault();
       $('#content').animate({
-        scrollLeft: "+=300px"
-      }, "slow");
+        scrollLeft: "+=150px"
+      }, "fast");
 });
    
 
 $('#left-button1').click(function() {
       event.preventDefault();
       $('#content1').animate({
-        scrollLeft: "-=300px"
-      }, "slow");
+        scrollLeft: "-=150px"
+      }, "fast");
 });
    
 $('#right-button1').click(function() {
       event.preventDefault();
       $('#content1').animate({
-        scrollLeft: "+=300px"
-      }, "slow");
+        scrollLeft: "+=150px"
+      }, "fast");
 });
    
 $('#left-button2').click(function() {
       event.preventDefault();
       $('#content').animate({
-        scrollLeft: "-=300px"
-      }, "slow");
+        scrollLeft: "-=500px"
+      }, "fast");
 });
    
 $('#right-button2').click(function() {
       event.preventDefault();
       $('#content2').animate({
-        scrollLeft: "+=300px"
-      }, "slow");
+        scrollLeft: "+=150"
+      }, "fast");
 });
    
